154a378a68102f4f7dac9a5e476c95964a677149
yashmunjal <yashmu45@gmail.com>
2020-06-05 23:58:00 +0530

    cross-env

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -8,7 +8,17 @@ const SearchParams = () => {
  const [breeds,setBreeds]=useState([]);
  const [animal,AnimalDropdown]=useDropdown("Animal", "dog",ANIMALS);
  const [breed,BreedDropdown,setBreed]=useDropdown("Breed","",breeds)
- 
+ const [pets,setPets]=useState([]);
+
+ async function requestPets(){
+   const {animals} = await pet.animals({
+    location,
+    breed,
+    type:animal
+   });
+
+   setPets(animals|| []);
+ }
 
  useEffect(()=>{
         setBreeds([]);
